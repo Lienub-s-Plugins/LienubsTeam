f39f4a45c61fff48b06b661d214c06a905462431
Lienub <lienub.68@gmail.com>
2024-06-15 23:51:02 +0200

    doc(listener): added javadoc for PlayerPositionListener class

--- src/main/java/lienub/dev/lienubsteam/listeners/PlayerPositionListener.java
+++ src/main/java/lienub/dev/lienubsteam/listeners/PlayerPositionListener.java
@@ -7,14 +7,35 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Listener for player position.
+ * This class listens for player position changes and notifies the player if they enter a team's territory.
+ *
+ * @see Listener
+ * @see PlayerMoveEvent
+ * @see Player
+ * @see Location
+ * @see LienubsTeam
+ */
 public class PlayerPositionListener implements Listener {
     private final LienubsTeam plugin;
 
+    /**
+     * Instantiates a new Player position listener.
+     *
+     * @param plugin the plugin
+     */
     public PlayerPositionListener(@NonNull LienubsTeam plugin) {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
+    /**
+     * On chunk enter.
+     *
+     * @param event the event
+     */
     @EventHandler
     public void onChunkEnter(PlayerMoveEvent event) {
         Player player = event.getPlayer();
