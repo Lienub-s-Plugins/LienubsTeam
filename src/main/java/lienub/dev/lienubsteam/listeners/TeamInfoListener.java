58c62bd845aed436647476458aed3ae53eb5edd0
Lienub <lienub.68@gmail.com>
2024-06-16 00:55:34 +0200

    feat(listener): using clicked item nbp to request tpa if its an item representing an online player

--- src/main/java/lienub/dev/lienubsteam/listeners/TeamInfoListener.java
+++ src/main/java/lienub/dev/lienubsteam/listeners/TeamInfoListener.java
@@ -2,11 +2,18 @@ package lienub.dev.lienubsteam.listeners;
 
 import lienub.dev.lienubsteam.LienubsTeam;
 import lienub.dev.lienubsteam.holders.TeamInfoHolder;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.NotNull;
 
@@ -49,12 +56,13 @@ public class TeamInfoListener implements Listener {
         event.setCancelled(true);
 
         ItemStack clickedItem = event.getCurrentItem();
+        Player player = (Player) event.getWhoClicked();
 
         // Change the page if the player clicked the next/previous page item.
-        itemLogic(clickedItem, inventory);
+        itemLogic(clickedItem, inventory, player);
     }
 
-    private void itemLogic(ItemStack clickedItem, Inventory inventory) {
+    private void itemLogic(ItemStack clickedItem, Inventory inventory, Player player) {
         if (clickedItem != null && clickedItem.getItemMeta() != null) {
             TeamInfoHolder holder = (TeamInfoHolder) inventory.getHolder();
 
@@ -71,6 +79,17 @@ public class TeamInfoListener implements Listener {
                     break;
                 default:
                     break;
+            }
+
+            // If item contains a nbt with key "onlinemember" then make the clicker request a tp to the player
+            ItemMeta meta = clickedItem.getItemMeta();
+            if (meta != null) {
+                PersistentDataContainer container = meta.getPersistentDataContainer();
+                NamespacedKey key = new NamespacedKey(JavaPlugin.getPlugin(LienubsTeam.class), "onlinemember");
+                if (container.has(key, PersistentDataType.STRING)) {
+                   player.performCommand("team tpa " + ChatColor.stripColor(meta.getDisplayName()));
+                   player.closeInventory();
+                }
             }
         }
     }
