b5e69380cd5d0f130c88be4ffe246656b05f18de
Lienub <lienub.68@gmail.com>
2024-06-16 00:00:05 +0200

    feat(listener): new inventory listener, and addition of behavior for team gui

---
+++ src/main/java/lienub/dev/lienubsteam/listeners/TeamInfoListener.java
@@ -0,0 +1,71 @@
+package lienub.dev.lienubsteam.listeners;
+
+import lienub.dev.lienubsteam.LienubsTeam;
+import lienub.dev.lienubsteam.holders.TeamInfoHolder;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Listens for inventory click events.
+ *
+ * @since 1.0
+ * @version 1.0
+ * @see Listener
+ * @see InventoryClickEvent
+ * @see Inventory
+ * @see ItemStack
+ * @see TeamInfoHolder
+ * @see LienubsTeam
+ * @see EventHandler
+ */
+public class TeamInfoListener implements Listener {
+    /**
+     * Instantiates a new Team info listener.
+     *
+     * @param plugin the plugin
+     */
+    public TeamInfoListener(@NonNull LienubsTeam plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    /**
+     * On inventory click.
+     *
+     * @param event the event
+     */
+    @EventHandler
+    public void onInventoryClick(@NotNull InventoryClickEvent event) {
+        // We're getting the clicked inventory to avoid situations where the player
+        // already has a stone in their inventory and clicks that one.
+        Inventory inventory = event.getInventory();
+        // Add a null check in case the player clicked outside the window.
+        if (!(inventory.getHolder() instanceof TeamInfoHolder)) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        ItemStack clickedItem = event.getCurrentItem();
+
+        // Change the page if the player clicked the next/previous page item.
+        if (clickedItem != null && clickedItem.getItemMeta() != null) {
+            TeamInfoHolder holder = (TeamInfoHolder) inventory.getHolder();
+            switch (clickedItem.getItemMeta().getDisplayName()) {
+                case "Page précédente":
+                    holder.previousPage();
+                    break;
+                case "Page suivante":
+                    holder.nextPage();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
