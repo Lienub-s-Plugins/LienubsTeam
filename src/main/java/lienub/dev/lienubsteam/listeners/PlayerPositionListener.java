73b490a78d02c496de53ea8c5ff2db1f9042945a
Lienub <lienub.68@gmail.com>
2024-06-16 02:09:38 +0200

    feat(listener):updating claim chunk bossbar everytime player enters a chunk

--- src/main/java/lienub/dev/lienubsteam/listeners/PlayerPositionListener.java
+++ src/main/java/lienub/dev/lienubsteam/listeners/PlayerPositionListener.java
@@ -1,12 +1,17 @@
 package lienub.dev.lienubsteam.listeners;
 
 import lienub.dev.lienubsteam.LienubsTeam;
+import lienub.dev.lienubsteam.utils.ClaimBossBarHolder;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.boss.KeyedBossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Listener for player position.
@@ -37,16 +42,17 @@ public class PlayerPositionListener implements Listener {
      * @param event the event
      */
     @EventHandler
-    public void onChunkEnter(PlayerMoveEvent event) {
+    public void onChunkEnter(@NotNull PlayerMoveEvent event) {
         Player player = event.getPlayer();
         Location from = event.getFrom();
         Location to = event.getTo() != null ? event.getTo() : from;
 
         if (!from.getChunk().equals(to.getChunk())) {
-            if (plugin.getTeamManager().getTeamByChunk(to.getChunk()) != null) {
-                player.sendMessage("You entered " + plugin.getTeamManager().getTeamByChunk(to.getChunk()).getName() + "'s territory");
-            } else {
-                player.sendMessage("You are in the wilderness");
+            NamespacedKey key = new NamespacedKey(plugin, "claimBossBar-" + player.getUniqueId());
+            KeyedBossBar bossBar = Bukkit.getBossBar(key);
+
+            if (bossBar != null) {
+                ClaimBossBarHolder.updateBossBar(bossBar, to.getChunk());
             }
         }
     }
